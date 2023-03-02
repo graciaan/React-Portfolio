9d57ce392f4c3d2f1ae8f655b1722fe9e94b8ae7
Andrew <andrewg87@gmail.com>
2023-03-02 09:57:15 -0500

    updated aboutme.js page, barebones - need to add more

---
+++ src/components/aboutMe.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+
+const aboutMe = () => {
+  return (
+    <div>
+      <p>
+        Hello! My name is Andrew Gracia and I am a full-stack web developer. 
+        I have experience with HTML, CSS, JavaScript, React.js, Express.js, Node.js, and with database languages like mySQL and MongoDB.
+        I am a fast learner and always working to expand my knowledge in everything I do.
+      </p>
+    </div>
+  )
+}
+
+export default aboutMe;
