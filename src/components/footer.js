e868840ae93881c850113f989d15bcaf381fb9b2
Andrew <andrewg87@gmail.com>
2023-03-02 20:26:46 -0500

    added code to footer, need to flesh out more

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>
+          Created by Andrew Gracia.
+          <a href='https://github.com/graciaan'>Github Page</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+//need to add more sites to link to, maybe twitter, gotta create a linkedin
+
+export default Footer;
