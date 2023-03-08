e930729b9a280db1d731a9a28aa3f508901dd5cc
Andrew <andrewg87@gmail.com>
2023-03-08 15:33:34 -0500

    added to resume.js

---
+++ src/components/resume.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+function Resume() {
+return (
+  <div>
+    <h1>
+      My Resume!
+    </h1>
+
+    <p>
+    I strongly consider myself a motivated person who is always interested in the latest technology. A detailed-oriented professional with a passion for software development, code reviews, data analysis, and project management. Strong knowledge in a wide variety of software, system architectures, and programming languages and always current with the latest developments in the software development world. Experienced in meeting deadlines and working under pressure with a high level of awareness and adaptability in fast-paced environments.
+    </p>
+
+    <h3>My Technical Skills</h3>
+    <ul>
+      <li>
+        Javascript
+      </li>
+      <li>
+        HTML
+      </li>
+      <li>
+        CSS
+      </li>
+      <li>
+        MySQL
+      </li>
+      <li>
+        React.js
+      </li>
+      <li>
+        Node.js
+      </li>
+      <h3>My Personal Skills</h3>
+      <li>
+        Created Problem Solver
+      </li>
+      <li>
+        Quick Learner
+      </li>
+      <li>
+        Team Leadership
+      </li>
+      <li>
+        Customer Service Expert
+      </li>
+      <li>
+        Data analysis
+      </li>
+    </ul>
+    <h3>Work History</h3>
+    <ul>
+      <li>
+        Office Assistant/Math Tutor
+      </li>
+      <li>
+        Logistics Specialist at Ceva Logistics
+      </li>
+      <li>
+        Inbound Sales Associate at TigerDirect
+      </li>
+      <li>
+        Assistant at City of Hialeah Law Department
+      </li>
+    </ul>
+  </div>
+)
+}
+export default Resume
